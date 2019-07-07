1bead5fa45255d973c71d99171b9b12ebce87d5e
Takayuki Uchida <jacobitebeerlove@gmail.com>
2019-07-08 01:00:41 +0900

    first commit

---
+++ Vagrantfile
@@ -0,0 +1,36 @@
+# coding: utf-8
+Vagrant.configure(2) do |config|
+
+  config.vm.box = "ubuntu/bionic64"
+
+  config.disksize.size = '256GB'
+
+  config.vm.define "nodeV1" do |node|
+    node.vm.provider "virtualbox" do |vb|
+      vb.name    = "develop"
+      vb.cpus    = 2
+      vb.memory  = 8192
+      vb.gui     = true
+      vb.customize [ "modifyvm"              , :id,
+                     "--natdnsproxy1"        , "on",
+                     "--natdnshostresolver1" , "on",
+                     "--nic1"                , "nat",
+                     "--nictype1"            , "virtio",
+                     "--vram"                , "256",
+                     "--clipboard"           , "bidirectional",
+                     "--draganddrop"         , "bidirectional",
+                     "--ioapic"              , "on"
+                   ]
+    end
+
+    node.vm.hostname = "develop"
+
+    node.vm.network "private_network", ip: "192.168.23.1", virtualbox__intnet: "intnet", nic_type: "virtio"
+
+    node.vm.provision "shell", path: "./build.sh", privileged: false
+
+    # node.vm.synced_folder "/Users/t-uchida/vagrant/share", "/vagrant", mount_options: ['dmode=755','fmode=744']
+    
+  end
+
+end
---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# packages
+sudo add-apt-repository -y ppa:kelleyk/emacs
+curl -s https://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg | sudo apt-key add -
+curl -s https://www.ubuntulinux.jp/ubuntu-jp-ppa-keyring.gpg | sudo apt-key add -
+sudo curl -s -o /etc/apt/sources.list.d/ubuntu-ja.list https://www.ubuntulinux.jp/sources.list.d/bionic.list
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install \
+        git \
+        curl \
+        tzdata \
+        emacs26 \
+        ubuntu-desktop \
+        ubuntu-defaults-ja \
+        chromium-browser \
+        nautilus-dropbox
+
+sudo apt-get -y purge firefox
+
+mkdir -p $HOME/bin $HOME/.local/bin
+
+# timezone
+#sudo ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+sudo timedatectl set-timezone Asia/Tokyo
+
+# locale
+sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja" 
+
+# dotfiles
+git clone https://github.com/chupaaaaaaan/dotfiles.git
+sudo ./dotfiles/deploy.sh
+
+# haskell stack
+curl -sSL https://get.haskellstack.org/ | sh
+
+# haskell ide engine
+# https://github.com/haskell/haskell-ide-engine
+sudo apt-get -y install libicu-dev libtinfo-dev libgmp-dev
+git clone https://github.com/haskell/haskell-ide-engine --recurse-submodules
+cd haskell-ide-engine
+stack ./install.hs hie-8.6.4
+stack ./install.hs build-doc   
+
