b270dfbea46580631c94b6644c42f9d8c00792d1
Takayuki Uchida <jacobitebeerlove@gmail.com>
2020-10-03 17:56:27 +0900

    いろいろ整理

--- build.sh
+++ build.sh
@@ -50,48 +50,48 @@ set -o pipefail
         sudo apt-get -y install emacs
 
         # firefox, thunderbirdは不要なので削除
-        sudo apt-get -y purge firefox thunderbird
+        #sudo apt-get -y purge firefox thunderbird
 
         # aptで入らないパッケージのインストール
         sudo snap install chromium
         sudo snap install discord
         sudo snap install libreoffice
 
-        # ソフトウェアセンターからインストールすると日本語入力できな問題があるので、debパッケージから直接インストールする。
-        #sudo snap install slack --classic
-        SLACK_VERSION=4.7.0 &&
-            curl -sSLO https://downloads.slack-edge.com/linux_releases/slack-desktop-${SLACK_VERSION}-amd64.deb &&
-            sudo dpkg -i slack-desktop-${SLACK_VERSION}-amd64.deb &&
-            rm -f slack-desktop-${SLACK_VERSION}-amd64.deb
+        # ソフトウェアセンターからインストールすると日本語入力できない問題があるので、debパッケージから直接インストールする。
+        #snap install slack --classic
+        curl -sSL -o /tmp/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-4.7.0-amd64.deb
+        sudo dpkg -i /tmp/slack.deb
     }
 
 
 : dotfileのインストール ||
     {
-        # dotfilesのインストール
         if [ -d "$HOME/dotfiles" ]; then
             ( cd $HOME/dotfiles && git pull )
         else
             git clone https://github.com/chupaaaaaaan/dotfiles.git
         fi
         $HOME/dotfiles/deploy.sh
-        rm -f $HOME/.profile && ln -s $HOME/.bash_profile $HOME/.profile
+
+        # Ubuntuの場合は.profileのみ読み込まれるため、.bash_profileのシンボリックリンクにする。
+        rm -f $HOME/.profile
+        ln -s $HOME/.bash_profile $HOME/.profile
+    }
+
+
+: AWS CLI v2のインストール ||
+    {
+        curl -sSL -o /tmp/awscliv2.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
+        unzip -u -d /tmp /tmp/awscliv2.zip
+        sudo /tmp/aws/install --update
     }
 
 
 : Terraformインストール ||
     {
-        # pip3のインストール
-        sudo apt-get -y install python3-pip
-
-        # awscliのインストール
-        pip3 install awscli --upgrade --user
-
-        # terraformのインストール
-        curl -sSLO https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip
-        unzip terraform_0.12.12_linux_amd64.zip
-        rm -f terraform_0.12.12_linux_amd64.zip
-        mv -f terraform bin/
+        curl -sSL -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip
+        unzip -u -d /tmp /tmp/terraform.zip
+        sudo mv -f /tmp/terraform /usr/local/bin/
     }
 
 
@@ -112,7 +112,7 @@ set -o pipefail
 
         # ghcupのインストール
         export BOOTSTRAP_HASKELL_NONINTERACTIVE=1
-        curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+        curl -sSL https://get-ghcup.haskell.org | sh
         . $HOME/.ghcup/env
 
         : > $HOME/.bash_profile.d/ghcup
@@ -127,75 +127,80 @@ set -o pipefail
 
         # ghcupにより、haskell-language-serverをインストール
         ghcup install hls latest
-
-        # パッケージインストール
-        # よく使うライブラリを入れておく(AtCoder用含む)
-        : Haskellインストール_パッケージ ||
-            {
-                cabal update
-
-                cabal install \
-                      hakyll \
-                      hlint \
-                      stylish-haskell
-
-                cabal install --lib \
-                      unicode-show
-
-                # AtCoder用
-                cabal install --lib \
-                      QuickCheck-2.13.2 \
-                      array-0.5.4.0 \
-                      attoparsec-0.13.2.3 \
-                      bytestring-0.10.10.0 \
-                      containers-0.6.2.1 \
-                      deepseq-1.4.4.0 \
-                      extra-1.7.1 \
-                      fgl-5.7.0.2 \
-                      hashable-1.3.0.0 \
-                      heaps-0.3.6.1 \
-                      integer-logarithms-1.0.3 \
-                      lens-4.19.1 \
-                      massiv-0.5.1.0 \
-                      mono-traversable-1.0.15.1 \
-                      mtl-2.2.2 \
-                      mutable-containers-0.3.4 \
-                      mwc-random-0.14.0.0 \
-                      parallel-3.2.2.0 \
-                      parsec-3.1.14.0 \
-                      primitive-0.7.0.1 \
-                      psqueues-0.2.7.2 \
-                      random-1.1 \
-                      reflection-2.1.5 \
-                      repa-3.4.1.4 \
-                      template-haskell-2.15.0.0 \
-                      text-1.2.4.0 \
-                      tf-random-0.5 \
-                      transformers-0.5.6.2 \
-                      unboxing-vector-0.1.1.0 \
-                      unordered-containers-0.2.10.0 \
-                      utility-ht-0.0.15 \
-                      vector-0.12.1.2 \
-                      vector-algorithms-0.8.0.3 \
-                      vector-th-unbox-0.2.1.7
-            }
-
-        : Haskellインストール_stack || 
-            {
-                sudo rm /usr/local/bin/stack -rf &&
-                    curl -sSL https://get.haskellstack.org/ | sh &&
-                    stack config set system-ghc --global true &&
-                    stack setup
-
-                : > $HOME/.bashrc.d/stack
-                echo 'eval "$(stack --bash-completion-script stack)"' >> $HOME/.bashrc.d/stack
-            }
     }
 
 
-: Node.js設定 ||
+: Haskellインストール_パッケージ ||
     {
-        # node.jsのインストール
+        cabal update
+
+        # アプリケーションのインストール
+        ghcup set ghc 8.8.4
+        cabal install --overwrite-policy=always \
+              hakyll \
+              hlint \
+              stylish-haskell
+
+        # ライブラリのインストール
+        ghcup set ghc 8.8.4
+        cabal install --lib \
+              unicode-show
+
+        # ライブラリのインストール (AtCoder用)
+        ghcup set ghc 8.8.3
+        cabal install --lib \
+              QuickCheck-2.13.2 \
+              array-0.5.4.0 \
+              attoparsec-0.13.2.3 \
+              bytestring-0.10.10.0 \
+              containers-0.6.2.1 \
+              deepseq-1.4.4.0 \
+              extra-1.7.1 \
+              fgl-5.7.0.2 \
+              hashable-1.3.0.0 \
+              heaps-0.3.6.1 \
+              integer-logarithms-1.0.3 \
+              lens-4.19.1 \
+              massiv-0.5.1.0 \
+              mono-traversable-1.0.15.1 \
+              mtl-2.2.2 \
+              mutable-containers-0.3.4 \
+              mwc-random-0.14.0.0 \
+              parallel-3.2.2.0 \
+              parsec-3.1.14.0 \
+              primitive-0.7.0.1 \
+              psqueues-0.2.7.2 \
+              random-1.1 \
+              reflection-2.1.5 \
+              repa-3.4.1.4 \
+              template-haskell-2.15.0.0 \
+              text-1.2.4.0 \
+              tf-random-0.5 \
+              transformers-0.5.6.2 \
+              unboxing-vector-0.1.1.0 \
+              unordered-containers-0.2.10.0 \
+              utility-ht-0.0.15 \
+              vector-0.12.1.2 \
+              vector-algorithms-0.8.0.3 \
+              vector-th-unbox-0.2.1.7
+
+        ghcup set ghc 8.8.4
+    }
+
+: Haskellインストール_stack ||
+    {
+        sudo rm /usr/local/bin/stack -f
+        curl -sSL https://get.haskellstack.org/ | sh
+        stack config set system-ghc --global true
+        stack setup
+
+        : > $HOME/.bashrc.d/stack
+        echo 'eval "$(stack --bash-completion-script stack)"' >> $HOME/.bashrc.d/stack
+    }
+
+
+: Node.jsインストール ||
+    {
         export NVM_DIR=$HOME/.nvm
         mkdir -p "$NVM_DIR"
         curl -sSL https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
@@ -205,7 +210,6 @@ set -o pipefail
         nvm install stable
         nvm alias default stable
 
-        # node.js用profileの作成
         : > $HOME/.bash_profile.d/node
         : > $HOME/.bashrc.d/node
         echo 'export NVM_DIR=$HOME/.nvm'                                        >> $HOME/.bash_profile.d/node
@@ -214,28 +218,37 @@ set -o pipefail
         echo '[ -s "$NVM_DIR/bash_completion" ] && \. $NVM_DIR/bash_completion' >> $HOME/.bashrc.d/node
 
         npm config set -g user root
+    }
 
-        : Elm設定 ||
-            {
-                # elmのインストール
-                npm install -g http-server elm elm-format elm-oracle elm-test @elm-tooling/elm-language-server
-            }
+
+: Elmインストール ||
+    {
+        npm install -g \
+            http-server \
+            elm \
+            elm-format \
+            elm-oracle \
+            elm-test \
+            @elm-tooling/elm-language-server
+    }
+
+
+: Java環境インストール ||
+    {
+        sudo apt-get -y install \
+             openjdk-11-jdk \
+             maven
+
+        : > $HOME/.bash_profile.d/jdk
+        echo '# export JAVA_HOME=`/usr/libexec/java_home -v 1.8`'
+        echo '# export JAVA_HOME=`/usr/libexec/java_home -v 11`'
+        echo 'export JAVA_HOME=$(echo $(readlink -f /usr/bin/javac) | sed -e "s:/bin/javac::")' >> $HOME/.bash_profile.d/jdk
     }
 
 
 : IntelliJ IDEAインストール ||
     {
-        # 必要なパッケージのインストール
-        sudo apt-get -y install openjdk-11-jdk maven
-
-        # JDK用profileの作成
-        : > $HOME/.bash_profile.d/jdk
-        echo '# export JAVA_HOME=`/usr/libexec/java_home -v 1.8`'
-        echo '# export JAVA_HOME=`/usr/libexec/java_home -v 11`'
-        echo 'export JAVA_HOME=$(echo $(readlink -f /usr/bin/javac) | sed -e "s:/bin/javac::")' >> $HOME/.bash_profile.d/jdk
-
-        # Intellij IDEAのインストール
-        sudo snap install intellij-idea-ultimate --classic
+        sudo snap install intellij-idea-community --classic
     }
 
 
